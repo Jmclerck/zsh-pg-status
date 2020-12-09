d728aae4e049698d85c561e0bd091479ff9ae2eb
Jon Clerck <j.m.clerck@icloud.com>
2020-12-09 06:17:41 +0000

    Hide status messages from pg status

--- zsh-pg-status.zsh
+++ zsh-pg-status.zsh
@@ -1,4 +1,4 @@
-pg=$(pg_ctl status --pgdata=/usr/local/var/postgresql@11 | rg -o 'PID: \d*' 1> /dev/null)
+pg=$(pg_ctl status --pgdata=/usr/local/var/postgresql@11 | rg -o 'PID: \d*' 2> /dev/null)
 
 if [[ -z $pg ]]; then
   pg_ctl start --pgdata=/usr/local/var/postgresql@11 1> /dev/null
