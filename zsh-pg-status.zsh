65075c14b7a8e32909290e51cae13cf060c444f1
Jon Clerck <j.m.clerck@icloud.com>
2020-12-09 06:10:12 +0000

    Initial commit 🎉

---
+++ zsh-pg-status.zsh
@@ -0,0 +1,5 @@
+pg=$(pg_ctl status --pgdata=/usr/local/var/postgresql@11 | rg -o 'PID: \d*' 1> /dev/null)
+
+if [[ -z $pg ]]; then
+  pg_ctl start --pgdata=/usr/local/var/postgresql@11 1> /dev/null
+fi
