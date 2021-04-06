16a646dbf66085290c44823e55cb825b28d0f3d9
Jon Clerck <jonc@biteable.com>
2021-04-06 09:29:02 +0100

    Use $PGDATA environment variable

--- zsh-pg-status.zsh
+++ zsh-pg-status.zsh
@@ -1,9 +1,9 @@
-pg=$(pg_ctl status --pgdata=/usr/local/var/postgresql 2> /dev/null)
+pg=$(pg_ctl status 2> /dev/null)
 
 if [[ -n $pg ]]; then
   pid=$(echo $pg | rg -o 'PID: \d*')
 
   if [[ -z $pid ]]; then
-    pg_ctl start --pgdata=/usr/local/var/postgresql 1> /dev/null
+    pg_ctl start 1> /dev/null
   fi
 fi
